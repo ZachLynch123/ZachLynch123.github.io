02d7154abb5ac580426c8772525cdad5e544c127
Zach <Zachary.Lynch1337@gmail.com>
2019-01-02 10:01:34 -0800

    Added formspree to contact page

--- email.js
+++ email.js
@@ -1,7 +1,24 @@
 console.log('Hello');
 
 $(document).ready(() => {
-    $('.submit').click(() => {
-        console.log('clicked!');
+    $('.submit').click((event) => {
+
+        const name = $('#name').val();
+        const email = $('#email').val();
+        const message = $('#message').val();
+
+        if (email.length > 5 && email.includes('@') && email.includes('.')) {
+            console.log('email valid');
+        } else {
+            console.log('invalid email');
+        // prevents form from submitting on failure
+            event.preventDefault();
+        }
+        if (message.length > 20) {
+            console.log('valid');
+        } else {
+            console.log('invalid');
+            event.preventDefault(); 
+        }
     })
 })
